a0de4b0dda2a9537203479fb6c1d1ec365615dd5
gokulakrishnan <gokulakrishnan145@gmail.com>
2022-05-09 09:58:21 +0530

    Update JsonApiTest.java

--- jsonapitest/src/main/java/com/jsonapi/example/JsonApiTest.java
+++ jsonapitest/src/main/java/com/jsonapi/example/JsonApiTest.java
@@ -8,11 +8,11 @@ public class JsonApiTest {
 		String jsonFilePath = "C:\\Users\\gokul\\Documents\\json\\StudentJson01.json";
 		System.out.println(
 				"KB: " + (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024);
-		System.out.println(Runtime.getRuntime().freeMemory());
+		
 		long start = System.currentTimeMillis();
 		//jsonApiStream.parse(jsonFilePath);
 jackson.parseJSON(jsonFilePath);
-		System.out.println(Runtime.getRuntime().freeMemory());
+		
 		System.out.println(
 				"KB: " + (double) (Runtime.getRuntime().totalMemory() - Runtime.getRuntime().freeMemory()) / 1024);
 		System.out.println("Total Time Taken : " + (System.currentTimeMillis() - start) + " milli secs");
